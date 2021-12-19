68e50f0984cc7adff79da6d115c6b9db0cf87611
Benedikt Schnatterbeck <benediktms@hey.com>
2021-12-19 18:15:05 +0000

    Add context user decorator to check logged in user

--- libs/auth/src/index.ts
+++ libs/auth/src/index.ts
@@ -1,2 +1,3 @@
 export * from './lib/auth.module';
 export * from './lib/guards/graphql-auth.guard';
+export * from './lib/decorators/ctx-user.decorator';
--- libs/core/src/lib/core.resolver.ts
+++ libs/core/src/lib/core.resolver.ts
@@ -1,12 +1,14 @@
-import { GraphQLAuthGuard } from '@grp-org/auth';
-import { UseGuards } from '@nestjs/common';
+import { CtxUser, GraphQLAuthGuard } from '@grp-org/auth';
+import { Logger, UseGuards } from '@nestjs/common';
 import { Float, Query, Resolver } from '@nestjs/graphql';
+import { User } from '@prisma/client';
 
 @Resolver()
 @UseGuards(GraphQLAuthGuard)
 export class CoreResolver {
   @Query(() => Float)
-  uptime() {
+  uptime(@CtxUser() user: User) {
+    Logger.log(user);
     return process.uptime();
   }
 }
