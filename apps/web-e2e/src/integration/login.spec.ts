9b13f3218c6ef2101d18c359e5f3502ea2ccce20
Benedikt Schnatterbeck <benediktms@hey.com>
2022-01-13 18:46:36 +0000

    Add e2e test to check a user can log in

---
+++ apps/web-e2e/src/integration/login.spec.ts
@@ -0,0 +1,41 @@
+import { aliasMutation, hasOperationName } from '../utils/graphql-test-utils';
+
+describe('web', () => {
+  const apiUrl = new URL(Cypress.env('apiUrl'));
+  const gqlEndpoint = new URL('/graphql', apiUrl);
+
+  beforeEach(() => {
+    cy.visit('/');
+    console.log('hello');
+
+    cy.intercept('POST', gqlEndpoint.href, (req) => {
+      if (hasOperationName(req, 'Login')) {
+        aliasMutation(req, 'Login');
+        req.reply((res) => {
+          res.body.data.login.user.firstName = 'John';
+        });
+      }
+    });
+  });
+
+  it('should be able to log in', () => {
+    cy.findByRole('button', { name: /login/i }).click();
+
+    cy.findByRole('button', { name: /login/i }).should('be.disabled');
+
+    cy.findByRole('textbox', { name: /email/i }).type('ben@example.com');
+    cy.findByLabelText(/password/i).type('helloworld');
+
+    cy.findByRole('button', { name: /login/i })
+      .should('not.be.disabled')
+      .click();
+
+    cy.wait('@gqlLoginMutation')
+      .its('response.body.data.login.user')
+      .should((user) => {
+        expect(user.firstName).to.equal('John');
+      });
+
+    cy.findByText(/login successful/i).should('exist');
+  });
+});
