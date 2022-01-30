f975ac31d9988796bd0d84af9273adebd20f31c2
Benedikt Schnatterbeck <benediktms@hey.com>
2022-01-30 16:07:35 +0000

    Try moving credentials to root client config

--- libs/client/data-access-gql/src/apollo/apolloClient.ts
+++ libs/client/data-access-gql/src/apollo/apolloClient.ts
@@ -1,4 +1,4 @@
-import { HttpLink, ApolloClient, InMemoryCache } from '@apollo/client';
+import { ApolloClient, InMemoryCache } from '@apollo/client';
 import { NextPageContext } from 'next';
 import { createWithApollo } from './createWithApollo';
 
@@ -7,17 +7,19 @@ import { createWithApollo } from './createWithApollo';
 export const createApolloClient = (_ctx: NextPageContext) => {
   const graphqlEndpoint = new URL('/graphql', process.env.NEXT_PUBLIC_API_URI);
 
-  const link = new HttpLink({
-    uri: graphqlEndpoint.href,
-    // headers: {
-    //   cookie: (isServer ? ctx?.req?.headers.cookie : undefined) || '',
-    // },
-    credentials: 'include',
-  });
+  // const link = new HttpLink({
+  //   uri: graphqlEndpoint.href,
+  //   // headers: {
+  //   //   cookie: (isServer ? ctx?.req?.headers.cookie : undefined) || '',
+  //   // },
+  //   // credentials: 'include',
+  // });
 
   return new ApolloClient({
-    link,
+    // link,
+    uri: graphqlEndpoint.href,
     cache: new InMemoryCache(),
+    credentials: 'include',
     // ssrMode: isServer,
   });
 };
